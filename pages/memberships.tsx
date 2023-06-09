28cffb4c14c869b044f3d0748f28bb295aa222fd
Tickether <tickether@gmail.com>
2023-06-09 12:18:12 +0000

    members + history Based!

---
+++ pages/memberships.tsx
@@ -0,0 +1,21 @@
+import styles from '@/styles/Memberships.module.css'
+import { NextPage } from 'next'
+import Footer from '@/components/footer'
+import Navbar from '@/components/navbar'
+
+
+const Memberships : NextPage = () => {
+  
+    
+    return (
+        <>
+            <Navbar/>
+                <div>
+                    <div></div>
+                </div>
+            <Footer/>
+        </>
+    )
+}
+
+export default  Memberships
---
+++ pages/transactions.tsx
@@ -0,0 +1,21 @@
+import styles from '@/styles/Transactions.module.css'
+import { NextPage } from 'next'
+import Footer from '@/components/footer'
+import Navbar from '@/components/navbar'
+
+
+const Transactions : NextPage = () => {
+
+
+  return (
+    <>
+      <Navbar/>
+        <div>
+          <div></div>
+        </div>
+      <Footer/>
+    </>
+  )
+}
+
+export default Transactions
